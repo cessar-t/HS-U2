f253a439d09ab6b69482c13d17ef35bee6ea84d5
cessar-t <146221903+cessar-t@users.noreply.github.com>
2025-10-09 16:00:20 -0600

    Delete codigo.py

--- codigo.py
+++
@@ -1,9 +0,0 @@
-def saludar(nombre):
-    return f"Hola {Grupo_de_herramientas}, este es un ejemplo de función en Python."
-
-def despedir(nombre):
-    return f"Adiós {Grupo_de_herramientas}, ¡nos vemos pronto!"
-
-if __name__ == "__main__":
-    print(saludar("Equipo"))
-    print(despedir("Equipo"))
